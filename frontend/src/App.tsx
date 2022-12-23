f2141d956db4b86b5e2f57505c93c93b5bddf9c5
OshadhaShiro <shirokumara49@gmail.com>
2022-12-23 23:14:14 +0530

    Setup basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import Header from './Components/Header/Header';
 import { useState } from 'react';
 import { useMantineTheme } from '@mantine/core';
+import Lessons from './pages/Lessons';
+import Games from './pages/Games';
 
 function App() {
 	const theme = useMantineTheme();
 	const [opened, setOpened] = useState(false);
+	// const navigate = useNavigate();
 
 	return (
 		<div>
 			<Header opened={opened} setOpened={setOpened} theme={theme} />
-
-			<Router>
-				<Routes>
-					<Route path="/" element={<Dashboard />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
-			</Router>
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/lessons" element={<Lessons />} />
+				<Route path="/games" element={<Games />} />
+			</Routes>
 		</div>
 	);
 }
