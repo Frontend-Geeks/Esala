b868f7296335ee1fb45cf6488f96c6d722e02ddb
OshadhaShiro <shirokumara49@gmail.com>
2022-12-23 23:15:00 +0530

    Setup Header

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,13 +1,18 @@
 import {
+	Text,
 	MediaQuery,
 	Burger,
 	Header as HeaderBar,
 	List,
 	Image,
 	Input,
+	Avatar,
 } from '@mantine/core';
 import logo from '../../assets/logo.png';
 import { IconSearch } from '@tabler/icons';
+import { Link } from 'react-router-dom';
+
+const NavbarElements = ['Home', 'Lessons', 'Games'];
 
 const Header = ({ opened, setOpened, theme }: any) => {
 	return (
@@ -18,6 +23,7 @@ const Header = ({ opened, setOpened, theme }: any) => {
 				background: '#292F33',
 				color: '#ffff',
 				borderBottomWidth: 0,
+				overflow: 'hidden',
 			}}
 		>
 			<div
@@ -36,56 +42,89 @@ const Header = ({ opened, setOpened, theme }: any) => {
 					alt="Esala Logo"
 				/>
 
-				<Input icon={<IconSearch />} placeholder="Search Subject, Grade" />
-
-				<List
-					sx={{
-						color: '#fff',
-						listStyle: 'none',
-						display: 'flex',
-						fontWeight: 500,
-						fontSize: '14px',
+				<Input
+					icon={<IconSearch />}
+					variant="filled"
+					placeholder="Search Subject, Grade"
+					styles={{
+						input: {
+							backgroundColor: '#111',
+							color: '#fff',
+							outline: 'none',
+							border: 'none',
+						},
 					}}
-				>
-					<List.Item
-						sx={{
-							margin: '0 15px 0 15px',
-							padding: '5px 10px 5px 10px',
-							borderRadius: '3px',
-							'&:hover': {
-								backgroundColor: 'rgba(0, 0, 0, 0.3)',
-							},
-						}}
-					>
-						Home
-					</List.Item>
-					<List.Item
-						sx={{
-							margin: '0 15px 0 15px',
-							padding: '5px 10px 5px 10px',
-							borderRadius: '3px',
-							'&:hover': {
-								backgroundColor: 'rgba(0, 0, 0, 0.3)',
-							},
-						}}
-					>
-						Lessons
-					</List.Item>
-					<List.Item
-						sx={{
-							margin: '0 15px 0 15px',
-							padding: '5px 10px 5px 10px',
-							borderRadius: '3px',
-							'&:hover': {
-								backgroundColor: 'rgba(0, 0, 0, 0.3)',
-							},
-						}}
-					>
-						Games
-					</List.Item>
-				</List>
+				/>
 
-				<MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+				<MediaQuery query="(max-width: 728px)" styles={{ display: 'none' }}>
+					<List
+						sx={{
+							listStyle: 'none',
+							display: 'flex',
+						}}
+					>
+						{NavbarElements.map((element) => (
+							<List.Item
+								sx={{
+									margin: '0 15px 0 15px',
+									padding: '5px 10px 5px 10px',
+									borderRadius: '3px',
+									'&:hover': {
+										backgroundColor: 'rgba(0, 0, 0, 0.3)',
+									},
+								}}
+							>
+								<Link
+									to={`/${element === 'Home' ? '' : element}`}
+									style={{
+										textDecoration: 'none',
+										fontWeight: 500,
+										fontSize: '14px',
+										color: '#fff',
+									}}
+								>
+									{element}
+								</Link>
+							</List.Item>
+						))}
+					</List>
+				</MediaQuery>
+
+				<MediaQuery query="(max-width: 728px)" styles={{ display: 'none' }}>
+					<div
+						style={{
+							width: '8rem',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'space-between',
+							background: '#111',
+							padding: '5px 12px 5px 12px',
+							borderRadius: '5px',
+						}}
+					>
+						<Text
+							sx={{
+								fontSize: '18px',
+								fontWeight: 500,
+							}}
+						>
+							Account
+						</Text>
+						<Avatar
+							src={null}
+							radius="xl"
+							size="md"
+							color="dark"
+							sx={{
+								display: 'flex',
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}
+						/>
+					</div>
+				</MediaQuery>
+
+				<MediaQuery query="(min-width: 728px)" styles={{ display: 'none' }}>
 					<Burger
 						opened={opened}
 						onClick={() => setOpened((o: boolean) => !o)}
