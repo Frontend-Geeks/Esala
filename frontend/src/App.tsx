1406b7b824a01360351cd85f4dc95036aae3dd96
OshadhaShiro <shirokumara49@gmail.com>
2022-12-24 21:17:17 +0530

    Setup Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,21 @@ import { useMantineTheme } from '@mantine/core';
 import Lessons from './pages/Lessons';
 import Games from './pages/Games';
 
+const data = {
+	NavElements: ['Home', 'Lessons', 'Games'],
+	DashboardItems: [
+		'Lesson 1',
+		'Lesson 2',
+		'Lesson 3',
+		'Lesson 4',
+		'Lesson 5',
+		'Lesson 6',
+		'Lesson 7',
+		'Lesson 8',
+		'Lesson 9',
+	],
+};
+
 function App() {
 	const theme = useMantineTheme();
 	const [opened, setOpened] = useState(false);
@@ -14,9 +29,9 @@ function App() {
 
 	return (
 		<div>
-			<Header opened={opened} setOpened={setOpened} theme={theme} />
+			<Header opened={opened} setOpened={setOpened} theme={theme} data={data} />
 			<Routes>
-				<Route path="/" element={<Dashboard />} />
+				<Route path="/" element={<Dashboard data={data} />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/lessons" element={<Lessons />} />
 				<Route path="/games" element={<Games />} />
