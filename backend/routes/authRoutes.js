c2a2e08a7ce1811132ac916755400de4c5544854
OshadhaShiro <shirokumara49@gmail.com>
2022-12-24 14:40:09 +0530

    Setup basic routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { createUser, loginUser } = require('../controllers/authControllers');
 
-router.post('/signup', createUser);
+router.post('/register', createUser);
 router.post('/login', loginUser);
 
 module.exports = router;
