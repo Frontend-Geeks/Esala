f6b9614fb071761792f4ac13fe50c14b0584b36a
OshadhaShiro <shirokumara49@gmail.com>
2022-12-27 23:59:07 +0530

    Remove react restrict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,16 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Header from './Components/Header/Header';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement,
 );
 root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
