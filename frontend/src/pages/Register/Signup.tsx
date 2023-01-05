3274c44719759fc11e50996a9bbccf709d79676a
OshadhaShiro <shirokumara49@gmail.com>
2023-01-05 23:28:56 +0530

    Signup Component

--- frontend/src/pages/Register/Signup.tsx
+++ frontend/src/pages/Register/Signup.tsx
@@ -1,44 +1,59 @@
 import { useState, useContext } from 'react';
 import { Input, Button, NavLink } from '@mantine/core';
-import { authContext } from '../../Context/auth/authContext';
-import { Navigate, useNavigate } from 'react-router';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
 const API_URL = 'api/auth/';
 
-const Signup = () => {
+const Signup = ({ styles }) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
 		password: '',
 	});
-
 	const { name, email, password } = formData;
-	const authCtx = useContext(authContext);
 	const navigate = useNavigate();
 
-	const handleSubmit = (e: any) => {
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
-		authCtx.signup(formData, () => {
-			navigate('/');
-		});
+		const response = await axios
+			.post('http://localhost:4000/api/auth/register', {
+				name: name,
+				email: email,
+				password: password,
+			})
+			.then((res) => {
+				if (res.data) {
+					localStorage.setItem('user', JSON.stringify(res.data));
+					navigate('/');
+				}
+				console.log(res);
+			})
+			.catch((err) => console.log(err));
 	};
 
 	return (
 		<>
 			<form>
-				<label>Name : </label>
-				<Input
-					type="text"
-					placeholder="Enter your name"
-					value={name}
-					onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-				/>
-				<label>Email : </label>
-				<Input
-					type="text"
-					placeholder="Enter your email"
-					value={email}
-					onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-				/>
+				<div className={styles.inputField}>
+					<label>Name : </label>
+					<Input
+						type="text"
+						placeholder="Enter your name"
+						value={name}
+						onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+					/>
+				</div>
+				<div className={styles.inputField}>
+					<label>Email : </label>
+					<Input
+						type="text"
+						placeholder="Enter your email"
+						value={email}
+						onChange={(e) =>
+							setFormData({ ...formData, email: e.target.value })
+						}
+					/>
+				</div>
 				<label>Password : </label>
 				<Input
 					type="password"
