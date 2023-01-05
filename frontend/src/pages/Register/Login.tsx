f89f3f102fbfe404e25ca81975c38bd2340b28b0
OshadhaShiro <shirokumara49@gmail.com>
2023-01-05 23:27:56 +0530

    Login component

--- frontend/src/pages/Register/Login.tsx
+++ frontend/src/pages/Register/Login.tsx
@@ -1,5 +1,40 @@
+import { Button, Input } from '@mantine/core';
+import { useState } from 'react';
+
 const Login = () => {
-	return <h1>Signup</h1>;
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const { email, password } = formData;
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+	};
+
+	return (
+		<form>
+			<label>Email : </label>
+			<Input
+				type="text"
+				placeholder="Enter your email"
+				value={email}
+				onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+			/>
+			<label>Password : </label>
+			<Input
+				type="password"
+				placeholder="Enter your password"
+				value={password}
+				onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+			/>
+
+			<Button type="submit" onClick={handleSubmit}>
+				Submit
+			</Button>
+		</form>
+	);
 };
 
 export default Login;
