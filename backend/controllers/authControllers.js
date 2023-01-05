3e4dab26158be196891ef00e46b5837b8ee641ce
OshadhaShiro <shirokumara49@gmail.com>
2023-01-05 23:25:31 +0530

    Firebase authentication

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,17 +1,47 @@
 const asyncHandler = require('express-async-handler');
-const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
+const {
+	getAuth,
+	createUserWithEmailAndPassword,
+	updateProfile,
+} = require('firebase/auth');
+const { getFirestore, collection, addDoc } = require('firebase/firestore');
 
 const auth = getAuth();
+const db = getFirestore();
 
 const createUser = asyncHandler(async (req, res) => {
-	const userResponse = await createUserWithEmailAndPassword(
-		auth,
-		'test1@test.com',
-		'abc1234#',
-	);
+	try {
+		const { name, email, password } = req.body;
 
-	console.log(userResponse);
-	res.send(userResponse);
+		if (!name || !email || !password) {
+			res.status(400);
+			throw new Error('Please add all fields');
+		}
+
+		const user = await createUserWithEmailAndPassword(auth, email, password)
+			.then(async (cred) => {
+				const docRef = await addDoc(collection(db, 'users'), {
+					userId: `${cred.user.uid}`,
+				});
+				await updateProfile(cred.user, {
+					displayName: name,
+				});
+			})
+			.catch((err) => console.log(err));
+
+		if (user.ok()) {
+			res.status(201).json({
+				_id: user.uid,
+				name: user.name,
+				email: user.email,
+			});
+		} else {
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(400);
+		throw new Error('Invalid user data');
+	}
 });
 
 const loginUser = asyncHandler(async (req, res) => {});
