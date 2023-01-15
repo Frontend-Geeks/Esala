c545a703b29dada48e61926f8d3b7fd3a38ad4f7
OshadhaShiro <shirokumara49@gmail.com>
2023-01-15 23:07:18 +0530

    Add login func

--- frontend/src/pages/Register/Login.tsx
+++ frontend/src/pages/Register/Login.tsx
@@ -1,7 +1,13 @@
 import { Button, Input } from '@mantine/core';
+import { useNavigate } from 'react-router';
 import { useState } from 'react';
+import useInput from '../../hooks/useInput';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const Login = () => {
+const Login = ({ styles }: any) => {
+	const navigate = useNavigate();
+
 	const [formData, setFormData] = useState({
 		email: '',
 		password: '',
@@ -9,26 +15,48 @@ const Login = () => {
 
 	const { email, password } = formData;
 
-	const handleSubmit = (e: any) => {
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
+
+		const reponse = await axios
+			.post('http://localhost:4000/api/auth/login', {
+				email: email,
+				password: password,
+			})
+			.then((res) => {
+				if (res.data) {
+					localStorage.setItem('user', JSON.stringify(res.data));
+				}
+				navigate('/');
+				toast.info(`Welcome back ${res.data.name}`);
+			})
+			.catch((err) => {
+				toast.error(err.response.data.errorMessage);
+			});
 	};
 
 	return (
 		<form>
-			<label>Email : </label>
-			<Input
-				type="text"
-				placeholder="Enter your email"
-				value={email}
-				onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-			/>
-			<label>Password : </label>
-			<Input
-				type="password"
-				placeholder="Enter your password"
-				value={password}
-				onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-			/>
+			<div className={styles.inputField}>
+				<label>Email : </label>
+				<Input
+					type="text"
+					placeholder="Enter your email"
+					value={email}
+					onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+				/>
+			</div>
+			<div className={styles.inputField}>
+				<label>Password : </label>
+				<Input
+					type="password"
+					placeholder="Enter your password"
+					value={password}
+					onChange={(e) =>
+						setFormData({ ...formData, password: e.target.value })
+					}
+				/>
+			</div>
 
 			<Button type="submit" onClick={handleSubmit}>
 				Submit
