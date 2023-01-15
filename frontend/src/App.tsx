d9cf8ce307e39532fde8fdabc5f2a1158c7f6cd8
OshadhaShiro <shirokumara49@gmail.com>
2023-01-15 23:04:22 +0530

    add react-toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register/Register';
 import Header from './Components/Header/Header';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useMantineTheme } from '@mantine/core';
 import Lessons from './pages/Lessons';
 import Games from './pages/Games';
 import './styles/App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const data = {
 	NavElements: ['Home', 'Lessons', 'Games'],
@@ -26,16 +28,39 @@ const data = {
 const App = () => {
 	const theme = useMantineTheme();
 	const [opened, setOpened] = useState(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (localStorage.getItem('user')) {
+			navigate('/');
+		} else {
+			navigate('/register');
+		}
+	}, [localStorage.getItem('user')]);
 
 	return (
 		<div>
 			<Header opened={opened} setOpened={setOpened} theme={theme} data={data} />
+
 			<Routes>
 				<Route path="/" element={<Dashboard data={data} />} />
 				<Route path="/lessons" element={<Lessons />} />
 				<Route path="/games" element={<Games />} />
 				<Route path="/register" element={<Register />} />
 			</Routes>
+
+			<ToastContainer
+				position="bottom-right"
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="dark"
+			/>
 		</div>
 	);
 };
